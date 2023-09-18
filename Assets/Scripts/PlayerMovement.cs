1a252d966bd4bed1f61a43cf05f76fbe9821e00b
hiroglyph22 <66212654+hiroglyph22@users.noreply.github.com>
2023-09-17 18:49:25 -0700

    Added project files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (rb.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
